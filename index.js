4e46dee79e07d18c93ce5a43d0aaf5f49a1d1c9b
osmar <osmarquintero87@gmail.com>
2024-06-15 19:18:15 -0600

    preloader view

---
+++ index.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let iframe = document.querySelector('iframe');
+    let preloader = document.getElementById('preloader_hojas');
+    let preloaderImg = document.getElementById('logo_ucn');
+
+    preloader.style.opacity = '1';
+    preloaderImg.style.opacity = '1';
+    
+
+    iframe.onload = function() {
+        setTimeout(() => {
+            preloader.style.opacity = '0';
+            preloader.style.transition = 'opacity 1s ease-in-out';
+            
+            preloaderImg.style.opacity = '0';
+            preloaderImg.style.transition = 'opacity 1s ease-in-out';
+        }, 2500);
+    };
+
+    // Listen for the transition to end before setting display to 'none'
+    preloader.addEventListener('transitionend', function(e) {
+        if (e.propertyName === 'opacity' && preloader.style.opacity === '0') {
+            preloader.style.display = 'none';
+            preloaderImg.style.display = 'none';
+        }
+    });
+});
